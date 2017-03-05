a7cf45c2252e626fd683c7bbaa2eb8e1d73ba56a
EhrenWilliamson <ehren.williamson2@gmail.com>
2017-03-05 15:37:56 -0600

    First commit

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,61 @@
+package LinkedList;
+
+public class LinkedList {
+	Node head = null;
+
+	public static void main(String[] args) {
+
+	}
+
+	public String search(String s) {
+		Node n = head;
+		while (n != null) {
+			System.out.println(n.getData());
+			n = n.next;
+		}
+		return s;
+
+	}
+
+	public Node getHead() {
+		return head;
+	}
+
+	public void traverse() {
+		Node n = head;
+		while (n != null) {
+			System.out.println(n.getData());
+			n = n.next;
+		}
+	}
+	
+	public void insert(Node newNode){
+		if(newNode == null){
+			newNode = new Node();
+			
+		}
+		if (head == null){
+			head = newNode;
+		}else {  newNode.next = head;
+		 	head=newNode;
+		
+		}
+	}
+	public void insertAfter(Node current, Node newNode){
+		newNode.next = current.next;
+		current.next = newNode;
+	}
+	public boolean delete(Node nodeToDelete){
+		Node n = head;
+		while (n != null){
+			n = n.next;
+			if(n == nodeToDelete){
+				n.next = nodeToDelete.next;
+				return true;
+			}
+
+			
+		}
+		return false;
+	}
+}
---
+++ src/LinkedList/Node.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class Node {
+	private String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+   Node next;
+}
